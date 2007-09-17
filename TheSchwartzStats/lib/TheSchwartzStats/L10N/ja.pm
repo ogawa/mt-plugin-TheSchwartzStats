27c995ae5b8d80a3a8a5486af088be44c3d0c417
Hirotaka Ogawa <hirotaka.ogawa@gmail.com>
2007-09-17 05:19:44 +0000

    l10n fix
    git-svn-id: file:///Users/ogawa/svn/mt-plugin-TheSchwartzStats/trunk@3 b7bee0e5-6241-4529-aa9e-08ae86e46850

--- TheSchwartzStats/lib/TheSchwartzStats/L10N/ja.pm
+++ TheSchwartzStats/lib/TheSchwartzStats/L10N/ja.pm
@@ -6,7 +6,7 @@ use base 'TheSchwartzStats::L10N';
 use vars qw( %Lexicon );
 
 our %Lexicon = (
-#    'Dashboard widget for showing TheSchwartz stats.' => 'TheSchwartzのステータスを表示するためのダッシュボード・ウィジェットです。',
+    'Dashboard widget for showing TheSchwartz stats.' => 'TheSchwartzのステータスを表示するためのダッシュボード・ウィジェットです。',
     'TheSchwartz Stats' => 'TheSchwartzのステータス',
     'Job Description' => 'ジョブの説明',
     'Remaining Jobs' => '残りジョブ数',
